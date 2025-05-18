1a60ccd931eff22464901064ce2f770b061039e8
John Schoeman <johnschoeman1617@gmail.com>
2025-05-18 12:51:27 -0400

    Pointers in C chp2

--- c/linked_list/main.c
+++ c/linked_list/main.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
 
-int j, k;
-int *ptr;
+
+// chp 2 : Pointer types and Arrays
 
 int main() {
-    j = 1;
-    k = 2;
-    ptr = &k;
-    printf("\n");
+    int my_array[] = {1, 2, 3, 4, 5};
+    int *ptr;
 
-    printf("j has the value %d and is stored at %p\n", j, (void *)&j);
-    printf("k has the value %d and is stored at %p\n", k, (void *)&k);
-    printf("ptr has the value %p and is stored at %p\n", ptr, (void *)&ptr);
-    printf("The value of the integer pointed to by ptr is %d\n", *ptr);
+    int i;
+    // ptr = &my_array[0];
+    ptr = my_array;
+
+    printf("\n\n");
+
+    for (i = 0; i < 6; i++) {
+      printf("my_array[%d] = %d   ", i, my_array[i]);
+      // printf("ptr + %d = %d\n", i, *(ptr + i));
+      printf("ptr + %d = %d\n", i, *(ptr++));
+      // printf("ptr + %d = %d\n", i, *(++ptr));
+    }
 
     return 0;
 }
+
+// chp 1 : What is a pointer?
+
+// int main() {
+//     j = 1;
+//     k = 2;
+//     ptr = &k;
+//     printf("\n");
+//     printf("j has the value %d and is stored at %p\n", j, (void *)&j);
+//     printf("k has the value %d and is stored at %p\n", k, (void *)&k);
+//     printf("ptr has the value %p and is stored at %p\n", ptr, (void *)&ptr);
+//     printf("The value of the integer pointed to by ptr is %d\n", *ptr);
+//     return 0;
+// }
